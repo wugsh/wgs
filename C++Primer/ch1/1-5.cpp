ce76744b69c08298fa882f6e04521121be6284b5
Gavin <wugsh15@163.com>
2018-11-29 12:49:01 +0800

    C++Primer
    Signed-off-by: Gavin <wugsh15@163.com>

---
+++ C++Primer/ch1/1-5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "输入两个数" << endl;
+
+	int v1, v2;
+	cin >> v1 >> v2;
+	cout << v1 << "和" << v2 << "的乘积为：" << v1 * v2 << endl;
+	return 0;
+}
