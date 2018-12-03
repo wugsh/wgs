76775e3bf46d25c1b7955c5d7877a78104f6815e
Gavin <wugsh15@163.com>
2018-12-03 16:14:46 +0800

    C++栈
    Signed-off-by: Gavin <wugsh15@163.com>

---
+++ C++Primer/DataStruct/栈/demo.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "stack.h"
+using namespace std;
+
+int main()
+{
+	Stack stack;
+	stack.Print();
+/*
+	stack.Push(1);
+	stack.Push(2);
+	stack.Push(3);
+	stack.Push(4);
+	*/
+	for (int i = 0; i < 100; i++)
+	{
+		stack.Push(i);
+	}
+	stack.Print();
+
+	cout << "****************" << endl;
+	stack.Pop();
+	stack.Print();
+	cout << "****************" << endl;
+	stack.Pop();
+	stack.Print();
+	cout << "****************" << endl;
+	stack.Pop();
+	stack.Print();
+	cout << "****************" << endl;
+	stack.Pop();
+	stack.Print();
+	cout << "****************" << endl;
+
+	return 0;
+}
