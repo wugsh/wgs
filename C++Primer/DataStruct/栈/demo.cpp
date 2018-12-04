acc667a887da156786acb560135fc60907e94b56
Gavin <wugsh15@163.com>
2018-12-04 23:49:06 +0800

    update
    Signed-off-by: Gavin <wugsh15@163.com>

--- C++Primer/DataStruct/栈/demo.cpp
+++ C++Primer/DataStruct/栈/demo.cpp
@@ -6,16 +6,17 @@ int main()
 {
 	Stack stack;
 	stack.Print();
-/*
+
 	stack.Push(1);
 	stack.Push(2);
 	stack.Push(3);
 	stack.Push(4);
-	*/
+	/*
 	for (int i = 0; i < 100; i++)
 	{
 		stack.Push(i);
 	}
+*/
 	stack.Print();
 
 	cout << "****************" << endl;
@@ -31,6 +32,12 @@ int main()
 	stack.Pop();
 	stack.Print();
 	cout << "****************" << endl;
+
+	Stack *s = new Stack;
+	s->Push(100);
+	s->Print();
+	s->Pop();
+	s->Print();
 
 	return 0;
 }
