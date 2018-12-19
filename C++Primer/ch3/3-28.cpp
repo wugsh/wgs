bf6444cee19108af101c97efc40fd2e79b0c7820
Gaosheng Wu <wugsh15@gmail.com>
2018-12-19 14:32:22 +0800

    update

---
+++ C++Primer/ch3/3-28.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+string sa[10];
+int ia[10];
+
+int main()
+{
+    string sa2[10];
+    int ia2[10];
+    
+    for (auto c : sa)
+        cout << c << " ";
+    cout << endl;
+
+    for (auto c : ia)
+        cout << c << " ";
+    cout << endl;
+
+    for (auto c : sa2)
+        cout << c << " ";
+    cout << endl;
+
+    for (auto c : ia2)
+        cout << c << " ";
+    cout << endl;
+    return 0;
+}
