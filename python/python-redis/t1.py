3e5d5e6b5b842e174fb0fde5e3f1538e2bf750fa
Gaosheng Wu <wugsh15@gmail.com>
2020-09-08 22:26:03 +0800

    python contorl redis

---
+++ python/python-redis/t1.py
@@ -0,0 +1,9 @@
+import redis   # 导入redis 模块
+
+
+pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)
+r = redis.Redis(host='localhost', port=6379, password='Cernet2019', decode_responses=True)
+r.set('name', 'runoob')  # 设置 name 对应的值
+print(r['name'])
+print(r.get('name'))  # 取出键 name 对应的值
+print(type(r.get('name')))  # 查看类型
